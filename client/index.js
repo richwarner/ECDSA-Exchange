096c9496cd23bfe73e2da1c9083d8e3136e72469
Rich Warner <richwarneriii@gmail.com>
2022-05-01 15:20:57 -0600

    App now creates a set of accounts and public-private key pairs on the server, and allows for transfers in the client UI using a private key or a signed message.

--- client/index.js
+++ client/index.js
@@ -15,13 +15,17 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   });
 });
 
+// Handle transfer
 document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const message = document.getElementById("message").value;
+  const senderPrivateKey = document.getElementById("sender-private-key").value;
+  const senderSignature = document.getElementById("sender-signature").value;
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, message, senderPrivateKey, senderSignature 
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
@@ -32,3 +36,31 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
     document.getElementById("balance").innerHTML = balance;
   });
 });
+
+// Handle signature generation
+document.getElementById("sign-create-signature").addEventListener('click', () => {
+  const senderPrivateKey = document.getElementById("sign-sender-private-key").value;
+  const message = document.getElementById("message").value;
+
+  const body = JSON.stringify({
+    senderPrivateKey, message 
+  });
+
+  const request = new Request(`${server}/sign`, { method: 'POST', body });
+
+  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
+    return response.json();
+  }).then(({ signature }) => {
+    document.getElementById("sign-signature-result").value = signature;
+    document.getElementById("sender-signature").value = signature;
+  });
+});
+
+
+// Handle message construction
+document.getElementById("send-amount").addEventListener('keyup', updateMessage);
+document.getElementById("recipient").addEventListener('keyup', updateMessage);
+
+function updateMessage() {
+  document.getElementById("message").value = `Send ${document.getElementById("send-amount").value} to ${document.getElementById("recipient").value}`; 
+}
--- server/index.js
+++ server/index.js
@@ -2,16 +2,41 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const secp = require('@noble/secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+// Set the total number of accounts
+const accountCount = 5;
+
+class keyPair {
+  constructor(privateKey, publicKey) {
+    this.privateKey = privateKey;
+    this.publicKey = publicKey;
+  }
+}
+const accounts = [];
+
+// Generate initial private-public key pairs and accounts
+for(let i = 0; i < accountCount; i++) {
+  // Generate one private key and convert it to hex
+  const privateKeyHex = secp.utils.bytesToHex(secp.utils.randomPrivateKey());
+  // Derive public key from private key and convert it to hex
+  const publicKeyHex = secp.utils.bytesToHex(secp.getPublicKey(privateKeyHex));
+  // Derive address from public key by [TODO: first hashing it and] using last 40 characters
+  const address = "0x" + publicKeyHex.slice(-40);
+  // Add key pair to accounts at address
+  accounts[address] = new keyPair(privateKeyHex, publicKeyHex);
+}
+
+// Set random initial balances for accounts
+const balances = {};
+for(const address in accounts) {
+  balances[address] = Math.round(Math.random() * Math.random() * 100);
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,12 +46,65 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  // console.log(req.body);
+  const {sender, recipient, amount, message, senderPrivateKey, senderSignature} = req.body;
+  if(isValidTransaction(sender, recipient, amount, message, senderPrivateKey, senderSignature)) {
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+    outputAccounts();
+  }
+  else console.log("Transaction isn't valid.");
+});
+
+app.post('/sign', (req, res) => {
+  const {senderPrivateKey, message} = req.body; 
+  (async () => {
+    const messageHash = await secp.utils.sha256(message);
+    const signature = await secp.sign(messageHash, senderPrivateKey);
+    // console.log("messageHash: " + messageHash);
+    // console.log("signature: " + signature);
+    res.send({ signature: secp.utils.bytesToHex(signature) });
+  })();
+
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+    console.log(`Listening on port ${port}!`); 
+    outputAccounts();
+  });
+
+// Authenticate the account, and otherwise check that it's a valid transaction
+function isValidTransaction(sender, recipient, amount, message, senderPrivateKey, senderSignature) {
+  const isValidAmount = amount >= 0;
+  const isValidSender = sender in balances;  console.log('isValidSender: ' + isValidSender);
+  const isValidBalance = amount <= balances[sender];  console.log('isValidAmount: ' + isValidAmount + ' amount:' + amount + ' balances[sender]: ' + balances[sender]);
+  const isValidPrivateKey = senderPrivateKey === accounts[sender].privateKey;  console.log('isValidPrivateKey: ' + isValidPrivateKey); console.log('senderPrivateKey: ' + senderPrivateKey + ' accounts[sender].privateKey:' + accounts[sender].privateKey);
+  const isValidSignature = secp.verify(senderSignature, SHA256(message).toString(), accounts[sender].publicKey); console.log('senderSignature: ' + senderSignature + ' messageHash: ' + SHA256(message).toString() + ' sender: ' + sender); console.log('isValidSignature: ' + isValidSignature);
+
+  return isValidAmount && isValidSender && isValidBalance && (isValidPrivateKey || isValidSignature); 
+}
+
+// Console.log the current account information
+function outputAccounts() {
+  let i = 0;
+
+  // Output available accounts (addresses) and balances
+  console.log(`
+Available Accounts
+==================`);
+  for(const address in balances) {
+    console.log(`(${i}) ${address} (${balances[address]} ETH)`); 
+    i++;
+  }
+
+  // Output private keys associated with those accounts
+  console.log(`
+Private Keys
+==================`);
+i = 0;
+  for(const address in accounts) {
+    console.log(`(${i}) ${accounts[address].privateKey}`); 
+    i++;
+  }
+}
